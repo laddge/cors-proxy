217d628c9fdbb51f90e69ffd99002e96db7d1e75
Laddge <dev.laddge@gmail.com>
2026-02-08 09:35:10 +0900

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+const app = new Hono()
+
+app.use(
+  '/api2/*',
+  cors({
+    origin: (origin) => (origin.endsWith('.laddge.net') ? origin : 'null')
+  })
+)
+
+app.get('/', async (c) => {
+  return await fetch(c.req.query('url'))
+})
+
+export default app
